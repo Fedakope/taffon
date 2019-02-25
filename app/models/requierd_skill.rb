1e3e0c864e75ac8dbf0a0f9bd76f651d82128f1b
Nil <nilbesombes@gmail.com>
2019-02-25 18:08:25 +0100

    model requierd_skill created

---
+++ app/models/requierd_skill.rb
@@ -0,0 +1,4 @@
+class RequierdSkill < ApplicationRecord
+  belongs_to :skill
+  belongs_to :job
+end
---
+++ test/models/requierd_skill_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RequierdSkillTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
