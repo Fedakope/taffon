7a8437685af99b948f8919587ddbb2fde82f2567
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-26 18:57:46 +0100

    simplify reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,60 +1,20 @@
 class ReviewsController < ApplicationController
-  before_action :set_apply, only: [create]
-  before_action :set_job, :set_organizer
-
-  def index
-    if @organizer
-      @reviews = Review.where(destinator_id: @organizer.id) # list all reviews of an organizer
-    elsif @user
-      @reviews = Review.where(destinator_id: @user.id) # list all reviews of an user
-    else
-      # reviews writted by current user
-      @reviews = Review.where(creator_id: current_user.id)
-      # reviews about current user...
-      @reviews = Review.where(destinator_id: current_user.id)
-    end
-  end
+  before_action :set_user
 
   def create
-    set_destinator
-    @review = Review.new(
-      review_params,
-      apply: @apply,
-      creator: current_user, # the current_user is always the creator of the review
-      destinator: @destinator
-    )
-    @review.save ? (redirect_to applies_path) : (render apply_path(@apply))
+    @review = Review.new(review_params)
+    @review.creator = current_user
+    @review.destinator = @user
+    @review.save ? (redirect_to root) : (render user_path(@user))
   end
 
   private
 
-  def set_destinator
-    if current_user.oragnizer # if the current user is an organizer he had to review the technician that do the job
-      @destinator = @apply.user
-    else
-      set_organizer # if current user isn't an organizer he had to review the organizer that create the job
-      @destinator = @organizer
-    end
-  end
-
-  def set_apply
-    @apply = Apply.find(params[:id])
-  end
-
-  def set_job
-    if @apply
-      @job = Job.find(@apply.job_id)
-    else
-      @job = Job.find(params[:id])
-    end
-  end
-
-  def set_organizer
-    event      = Event.find(@job.event_id)
-    @organizer = User.find(event.user_id)
-  end
-
   def review_params
     params.require(:review).permit(:rating, :content) # strong params
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
