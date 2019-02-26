6dd631d0fdcd6f718301c6557f7a9fd315936cdc
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-26 15:31:24 +0100

    create sub controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,24 @@
 class ReviewsController < ApplicationController
-
   def index
 
   end
 
   def create
+
+  end
+
+  private
+
+  def find_job
+
+  end
+
+  def find_apply
+
+  end
+
+
+  def find_organizer
 
   end
 end
