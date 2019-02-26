05346ef62a3a234ba19dcceb6d4515e6f01532be
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-02-26 15:34:01 +0100

    Organizer Event Controller
    View pages: Index, Show

---
+++ app/controllers/organizer/events_controller.rb
@@ -0,0 +1,24 @@
+class Organizer::EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
