75a37e06b4f9a19e88895344a152de4668c0a684
MaxKrak <maximelegras@hotmail.com>
2019-02-26 19:05:52 +0100

    seeds part 2 with destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'faker'
 
-
+Event.destroy_all
 User.destroy_all
+
+
 
 organizer1 = User.create!(
   email: "organizer1@gmail.com",
@@ -41,9 +43,9 @@ events = []
   users << User.create!(
     email: Faker::Internet.email,
     password: "azerty",
-    first_name: "tech1-firstname",
-    last_name: "tech1-lastname",
-    phone_number: 0424242426,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: Faker::Number.leading_zero_number(10),
     organizer: n.odd?)
 end
 
@@ -52,11 +54,11 @@ organizer_users = users.select {|u| u.organizer }
 organizer_users.each do |u|
   rand(5).times do |n|
   events << Event.create!(
-    name: "event1",
-    full_address: "adress...",
-    description: "description...",
+    name: Faker::Superhero.descriptor,
+    full_address: Faker::Address.full_address,
+    description: Faker::Marketing.buzzwords,
     picture_url: "picture_url...",
-    company: "company...",
+    company: Faker::Company.name,
     user_id: u.id,
     start_date: 1.day.from_now,
     end_date: 2.day.from_now)
