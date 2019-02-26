e9fb3ebf091ab3acf89d3136dc77651d75de4271
Nil <nilbesombes@gmail.com>
2019-02-26 12:59:26 +0100

    fixing model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_201826) do
+ActiveRecord::Schema.define(version: 2019_02_26_101947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,19 @@ ActiveRecord::Schema.define(version: 2019_02_25_201826) do
     t.index ["skill_id"], name: "index_required_skills_on_skill_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "apply_id"
+    t.bigint "creator_id"
+    t.bigint "destinator_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["apply_id"], name: "index_reviews_on_apply_id"
+    t.index ["creator_id"], name: "index_reviews_on_creator_id"
+    t.index ["destinator_id"], name: "index_reviews_on_destinator_id"
+  end
+
   create_table "skills", force: :cascade do |t|
     t.string "name"
     t.string "category"
@@ -101,4 +114,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_201826) do
   add_foreign_key "jobs", "events"
   add_foreign_key "required_skills", "jobs"
   add_foreign_key "required_skills", "skills"
+  add_foreign_key "reviews", "applies"
+  add_foreign_key "reviews", "users", column: "creator_id"
+  add_foreign_key "reviews", "users", column: "destinator_id"
 end
