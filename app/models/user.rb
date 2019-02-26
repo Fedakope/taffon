50f2c4a62bb4b817f1f8b380bc241abae6d9d32a
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-26 19:05:16 +0100

    fix has_many relations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_many :applies
   has_many :events
   has_many :skills, through: :have_skill
+  has_many :reviews, foreign_key: :creator_id
+  has_many :reviews, foreign_key: :destinator_id
 end
