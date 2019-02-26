5ddc7c9b56885861f37709a3ca95ba06e48fa969
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-26 15:10:59 +0100

    add route for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
+
   devise_for :users
 
   namespace :organizer do
-    resources :events do 
+    resources :events do
       resources :jobs, only: [:create]
     end
     resources :jobs, only: [:destroy] do
       resources :applies, only: [:index]
     end
-      resources :applies, only: [] do 
+      resources :applies, only: [] do
         patch :accept
         patch :decline
       end
   end
 
-  resources :jobs, only: [:index, :show] do 
+  resources :jobs, only: [:index, :show] do
     resources :applies, only: [:create]
   end
 
-  resources :applies, only: [:index, :destroy]
+  resources :applies, only: [:index, :destroy] do
+    # Routes for Reviews :
+    resources :reviews, only: [:create]
+  end
 end
