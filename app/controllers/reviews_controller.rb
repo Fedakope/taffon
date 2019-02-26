dab8ca0949f7e5f8a4f7351cbecff9fce33d946f
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-26 16:08:10 +0100

    add set_destinator of the review and finish the create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,50 @@
 class ReviewsController < ApplicationController
-  def index
+  before_action :set_apply, :set_job, :set_organiser
 
+  def index
+    set_organizer
+    @reviews = Review.where(destinator_id: @organizer.id)
   end
 
   def create
-
+    @review = Review.new(review_params)
+    set_destinator
+    @review = {
+      apply: @apply,
+      creator: @current_user, # the current_user is always the creator of the review
+      destinator: @destinator
+    }
   end
 
   private
 
-  def find_job
-
+  # set the destinator of the review
+  def set_destinator
+    if @creator == @organizer
+      @destinator = @apply.user # if the current user is an organizer he had to review the technician
+    else
+      @destinator = @organizer # if current user isn't an organizer he had to review the organizer
+    end
   end
 
-  def find_apply
-
+  # set variables
+  def set_apply
+    @apply = Apply.find(params[:id])
   end
 
+  def set_job
+    # set_apply
+    @job = Job.find(@apply.job_id)
+  end
 
-  def find_organizer
+  def set_organizer
+    # set_job
+    event = Event.find(@job.event_id)
+    @organizer = User.find(event.user_id)
+  end
 
+  # strong params
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
