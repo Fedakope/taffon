1bcdb3a208cd23a21991d6854c11a7a065931540
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-02-27 08:57:28 +0100

    Bugfix on the initializer for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,4 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
-  p ENV['GOOGLE_CLIENT_ID']
-  p ENV['GOOGLE_CLIENT_SECRET']
 end
