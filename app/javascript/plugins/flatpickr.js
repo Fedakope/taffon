54aca4eb33aa5130f4d51b95ec50ad2fec462d51
Nil <nilbesombes@gmail.com>
2019-02-27 22:40:25 +0100

    human readable pour new event

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,11 +2,19 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
 flatpickr(".datepicker", {
-  altInput: true
+  altInput: true,
+  altFormat: "j F, Y",
+  dateFormat: "Y-m-d",
+  minDate: "today",
+  defaultDate: ["today"]
+
 })
 
 
 flatpickr(".datetimepicker", {
-  enableTime: true
-  altInput: true
+  enableTime: true,
+  dateFormat: "Y-m-d H:i",
+  minDate: "today",
+  defaultDate: ["today"]
+
 })
