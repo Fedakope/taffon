b40e89633562328145db6aa48cd5e303b7c892bd
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-02-27 11:52:02 +0100

    add has_many: :have_skill relation in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :applies
   has_many :events
+  has_many :have_skill
   has_many :skills, through: :have_skill
   has_many :reviews, foreign_key: :creator_id
   has_many :reviews, foreign_key: :destinator_id
