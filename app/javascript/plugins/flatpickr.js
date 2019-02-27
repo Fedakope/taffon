0386b0bf94f8b4452f99bb18f907a88a0677638a
Nil <nilbesombes@gmail.com>
2019-02-27 21:46:43 +0100

    Ok date sur Event et date + time sur job

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,3 +2,8 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
 flatpickr(".datepicker", {})
+
+
+flatpickr(".datetimepicker", {
+      enableTime: true
+})
