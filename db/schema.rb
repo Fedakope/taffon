8bb866ea1efb0e274fa863d6380b0d05d9f502f2
Nil <nilbesombes@gmail.com>
2019-02-27 11:56:35 +0100

    added tech or organizer to signup page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_02_26_180049) do
 
   # These are extensions that must be enabled in order to support this database
