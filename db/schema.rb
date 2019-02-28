934d18cf1897c7ac5036e77cc4cec75abe16121f
MaxKrak <maximelegras@hotmail.com>
2019-02-28 20:56:03 +0100

    show organizer event corrected

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_141426) do
+ActiveRecord::Schema.define(version: 2019_02_28_163644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2019_02_28_141426) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "description"
-    t.string "category"
     t.bigint "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -96,7 +95,6 @@ ActiveRecord::Schema.define(version: 2019_02_28_141426) do
     t.boolean "organizer", default: false
     t.string "provider"
     t.string "uid"
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
