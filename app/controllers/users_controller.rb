76883b00c77f1f0af71d76c4d6ae2c671a0c0c71
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-03-01 11:27:09 +0100

    Added Welcome email when user sign up!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
+  # after_create :send_welcome_mail
+  
   def index
     @users = User.all
     if params[:query].present?
       sql_query = " \
-        full_address ILIKE :query"
+      full_address ILIKE :query"
       @users = User.where(sql_query, query: "%#{params[:query]}%")
     else
       @users = User.all
     end
   end
-
+  
   def show
     @user = User.find(params[:id])
     @reviews = Review.where(destinator: @user)
     @review = Review.new
   end
+  
+  # private
+  
+  # def send_welcome_mail
+  #   user = User.last
+  #   UserMailer.welcome(user).deliver_now
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", :registrations => "my_registrations" }
+  
   devise_for :users, only: [:edit, :show]
 
   resources :users, only: [:index, :show] do
