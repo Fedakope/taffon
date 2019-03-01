b64518d155a01a9319ac872ccd301ac9f04c30f9
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-03-01 09:53:39 +0100

    Added flash message for double skill

--- app/controllers/have_skills_controller.rb
+++ app/controllers/have_skills_controller.rb
@@ -3,7 +3,10 @@ class HaveSkillsController < ApplicationController
     @have_skill = HaveSkill.new
     @have_skill.skill_id = params['/profile']['skill'].to_i
     @have_skill.user = current_user
-    @have_skill.save
-    redirect_to profile_path
+    if @have_skill.save
+      redirect_to profile_path
+    else 
+      redirect_to profile_path, alert: "You already added that skill"
+    end
   end
 end
