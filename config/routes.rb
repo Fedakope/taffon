52d156e6ea6cbb908794b259fb50ff1e12fb3dd4
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-03-01 12:26:08 +0100

    generate notification model ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Notifications::Engine => "/notifications"
   root to: "pages#home"
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
