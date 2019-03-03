de36bbc1b14379cc3bda01a2ec89a6bf624ebce7
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-03-03 14:25:50 +0100

    Events page (index) layout
    HTML & CSS

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,12 +1,14 @@
 import Typed from 'typed.js';
-
+const banner = document.querySelector('#banner-typed-text');
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["the right event", "the right organizer", "or the right team"],
-    typeSpeed: 100,
-    loop: true,
-    loopCount: Infinity
-  });
+  if (banner) {
+    new Typed('#banner-typed-text', {
+      strings: ["the right event", "the right organizer", "or the right team"],
+      typeSpeed: 100,
+      loop: true,
+      loopCount: Infinity
+    });
+  }
 };
 
 export { loadDynamicBannerText };
