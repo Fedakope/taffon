bb1cbce0987eff0b01bae9ec00db02c485a3f620
Nil <nilbesombes@gmail.com>
2019-03-04 10:25:37 +0100

    display job listing homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @jobs = Job.order("RANDOM()").limit(3)
+    @users = User.where(["organizer = false"]).order("RANDOM()").limit(3)
   end
 
   def profile
     @have_skills = HaveSkill.where(user_id: current_user.id)
   end
 end
+
+
