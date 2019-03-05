6fea1486fe3ee40fdfd8a765684081c9b1bebb85
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-03-05 12:11:39 +0100

    Added markers for the map in the technicians list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    set_markers
     @users = User.all
     if params[:query].present?
       sql_query = " \
@@ -17,4 +18,15 @@ class UsersController < ApplicationController
     @apply = Apply.new
   end
 
+  private
+
+  def set_markers
+    @events = Event.where.not(latitude: nil, longitude: nil)
+    @markers = @events.map do |event|
+      {
+        lng: event.longitude,
+        lat: event.latitude,
+      }
+    end
+  end
 end
