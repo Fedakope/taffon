ca24203fbafa8e9c9e9d2eade338ecb757807614
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-03-05 14:07:38 +0100

    Revert "Pictures update everywhere"
    This reverts commit 050673d1544bce86d4cc744c8eeca511fb6e3741.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_110048) do
+ActiveRecord::Schema.define(version: 2019_03_04_093821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,8 +115,6 @@ ActiveRecord::Schema.define(version: 2019_03_05_110048) do
     t.string "full_address"
     t.string "picture_url"
     t.string "description"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
