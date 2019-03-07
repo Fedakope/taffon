d81236ffaa719a279cddb3bbfc048e7ea78e9f8e
Alexis Dewerdt <alexis.dewerdt@gmail.com>
2019-03-07 13:37:58 +0100

    fix all notification system view

--- app/controllers/notifications/notifications_controller.rb
+++ app/controllers/notifications/notifications_controller.rb
@@ -1,17 +1,28 @@
 module Notifications
   class NotificationsController < Notifications::ApplicationController
     def index
-      @notifications = notifications.includes(:actor).order('id desc').page(params[:page])
-
-      unread_ids = @notifications.reject(&:read?).select(&:id)
-      Notification.read!(unread_ids)
+      read_all
 
       @notification_groups = @notifications.group_by { |note| note.created_at.to_date }
     end
 
     def clean
       notifications.delete_all
-      redirect_to notifications_path
+      redirect_to profile_path
+    end
+
+    def destroy
+      @notification = Notification.find(params[:id])
+      @notification.destroy
+      redirect_to main_app.profile_path
+    end
+
+
+    def read_all
+      @notifications = notifications.includes(:actor).order('id desc').page(params[:page])
+      unread_ids = @notifications.reject(&:read?).select(&:id)
+      Notification.read!(unread_ids)
+      redirect_to main_app.profile_path
     end
 
     private
@@ -20,5 +31,7 @@ module Notifications
       raise 'You need reqiure user login for /notifications page.' unless current_user
       Notification.where(user_id: current_user.id)
     end
+
+
   end
 end
