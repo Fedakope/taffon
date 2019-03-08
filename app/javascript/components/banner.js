deb67c67328d586b5a56f60a413a9080398f1962
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2019-03-08 11:31:15 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,9 +3,10 @@ const banner = document.querySelector('#banner-typed-text');
 const loadDynamicBannerText = () => {
   if (banner) {
     new Typed('#banner-typed-text', {
-      strings: ["le bon évènement", "le bon organisateur", "la bonne équipe"],
-      typeSpeed: 150,
+      strings: ["le bon technicien ^900","^900 le bon évènement ^900", "^900 le bon organisateur ^900", "^900 la bonne équipe ^900"],
+      typeSpeed: 130,
       loop: true,
+      backSpeed: 50,
       loopCount: Infinity
     });
   }
